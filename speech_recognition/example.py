356e7d5d30c75bd09885f3de6a230e370c679b81
KEHANG <kehanghan@gmail.com>
2022-11-30 21:24:24 -0800

    Adds an example of speech recognition

---
+++ speech_recognition/example.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
